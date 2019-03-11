715786dc8862826c8b29b67d8d59f6c372b7b5fb
张常乐 <changle0331@gmail.com>
2019-03-11 18:45:15 +0800

    Update NumArray1.java

--- LeetCode/303. Range Sum Query - Immutable/src/NumArray1.java
+++ LeetCode/303. Range Sum Query - Immutable/src/NumArray1.java
@@ -30,8 +30,3 @@ class NumArray {
     }
     
 }
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(i,j);
- */
