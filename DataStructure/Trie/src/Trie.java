1023703ed9cc06ec81c90599699f3c71439fa9b9
changle <changle0331@gmail.com>
2019-03-13 00:38:21 +0800

    DataStructure——Trie：Trie Added

--- DataStructure/Trie/src/Trie.java
+++ DataStructure/Trie/src/Trie.java
@@ -2,9 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author Changle
- * @date 2019/3/12 15:21
- * source: https://leetcode.com/problems/implement-trie-prefix-tree/
+ * @author: changle
+ * @time: 2019-03-13 00:27
  */
 
 class Trie {
@@ -26,13 +25,11 @@ class Trie {
         }
     }
 
-    /** Initialize your data structure here. */
     public Trie() {
         root = new Node();
     }
 
-    /** Inserts a word into the trie. */
-    public void insert(String word) {
+    public void add(String word) {
         Node cur = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
@@ -47,8 +44,7 @@ class Trie {
         }
     }
 
-    /** Returns if the word is in the trie. */
-    public boolean search(String word) {
+    public boolean contains(String word) {
         Node cur = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
@@ -61,8 +57,7 @@ class Trie {
         return cur.isWord;
     }
 
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    public boolean startsWith(String prefix) {
+    public boolean isPrefix(String prefix) {
         Node cur = root;
         for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
@@ -74,11 +69,3 @@ class Trie {
         return true;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
