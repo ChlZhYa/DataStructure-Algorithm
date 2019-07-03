e3aca9015c2d490eb0b6edf529e0750fff98958b
张常乐 <changle0331@gmail.com>
2019-07-03 10:07:30 +0800

    Update Solution.java
    26-V1.1

--- LeetCode/26. Remove Duplicates from Sorted Array/src/Solution.java
+++ LeetCode/26. Remove Duplicates from Sorted Array/src/Solution.java
@@ -1,24 +1,26 @@
 /**
- * User: Changle
- * Date: 2018-02-08 09:19
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ * @author Changle
+ * @date 2019/6/27 9:46
+ * source: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * T(n) : O(n)
+ * S(n) : O(1)
+ * Tag  : Double Pointer
  */
 
-/*
-不允许使用额外的空间复杂度，所以应该是在数组内交换位置，采用双指针。
- i 指向第一个确定值的元素。
- j 指针扫描数组，当遇到与前一个数不同时，就把这个新的数放到 ++i 位置上。
-
-时间复杂度O(n)
-空间复杂度O(1)
- */
 public class Solution {
+
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for(int j = 1;j < nums.length;j++){
-            if(nums[j] != nums[j - 1])
-                nums[++i] = nums[j];
+        if (nums.length == 0) {
+            return 0;
         }
-        return i + 1;
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] > nums[left]) {
+                nums[++left] = nums[right++];
+            } else {
+                right++;
+            }
+        }
+        return left + 1;
     }
 }
