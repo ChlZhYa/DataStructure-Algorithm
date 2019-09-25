22e69e7e7dbe36eef4a7f4203f944d144d7ea199
changle <changle0331@gmail.com>
2019-09-26 03:12:02 +0800

    0198 updated

--- LeetCode/0198. House Robber/src/Solution2.java
+++ LeetCode/0198. House Robber/src/Solution2.java
@@ -4,5 +4,16 @@
  * resource: https://leetcode.com/problems/house-robber/
  */
 public class Solution2 {
+    public int rob(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        };
 
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            dp[i] = Math.max(dp[i - 1],(i - 2 < 0 ? nums[i] : (dp[i - 2] + nums[i])));
+        }
+        return dp[nums.length - 1];
+    }
 }
