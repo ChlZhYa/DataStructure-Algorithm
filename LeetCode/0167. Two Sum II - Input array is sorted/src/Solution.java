14bd61dcac68b2dd4d31be8eb3ee3b08d9a4370c
changleAmazing <changle0331@gmail.com>
2019-11-23 12:50:31 +0800

    0167. Two Sum II - Input array is sorted V1.1

--- LeetCode/0167. Two Sum II - Input array is sorted/src/Solution.java
+++ LeetCode/0167. Two Sum II - Input array is sorted/src/Solution.java
@@ -4,27 +4,29 @@
  * Source: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
  */
 
-/*
-由于数组有序，使用对撞指针。
-当两个指针处的数字之和大于 target ，则 j 向左移动。
-反之 i 向右移动。
-
-时间复杂度：O(n)
-空间复杂度: O(1)
+/**
+ * 由于数组有序，使用对撞指针。
+ * 当两个指针处的数字之和大于 target ，则 j 向左移动。
+ * 反之 i 向右移动。
+ *
+ * 时间复杂度：O(n)
+ * 空间复杂度: O(1)
  */
 public class Solution {
+
     public int[] twoSum(int[] numbers, int target) {
-        int i = 0,j = numbers.length - 1;
+        int i = 0, j = numbers.length - 1;
         int[] res = new int[2];
-        while(i < j){
-            if(numbers[i] + numbers[j] == target){
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target) {
                 res[0] = i + 1;
                 res[1] = j + 1;
                 break;
-            }else if(numbers[i] + numbers[j] < target)
+            } else if (numbers[i] + numbers[j] < target) {
                 i++;
-            else
+            } else {
                 j--;
+            }
 
         }
         return res;
