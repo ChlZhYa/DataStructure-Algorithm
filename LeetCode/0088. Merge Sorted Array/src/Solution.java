71f6136d98f152854a5b80ffbb30184f21b5ac8f
changleAmazing <changle0331@gmail.com>
2019-11-23 12:32:14 +0800

    0088. Merge Sorted Array V1.1

--- LeetCode/0088. Merge Sorted Array/src/Solution.java
+++ LeetCode/0088. Merge Sorted Array/src/Solution.java
@@ -4,12 +4,12 @@
  * Source: https://leetcode.com/problems/merge-sorted-array/description/
  */
 
-/*
-由于题目中说明 nums1 的长度为 m+n，可以容纳两个数组的所有元素。
-所以可以考虑从 nums1 的数组尾部开始填充大数。
-
-时间复杂度：O(n)
-空间复杂度：O(1)
+/**
+ * 由于题目中说明 nums1 的长度为 m+n，可以容纳两个数组的所有元素。
+ * 所以可以考虑从 nums1 的数组尾部开始填充大数。
+ *
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
  */
 public class Solution {
 
@@ -34,34 +34,3 @@ public class Solution {
     }
 }
 
-/*
-很容易想到归并，所以使用一个额外数组存放。
-
-时间复杂度：O(n)
-空间复杂度：O(n)
- */
-class Solution1 {
-
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m + n];
-        int i = 0;
-        int j = 0;
-        for (int k = 0; k < res.length; k++) {
-            if (i >= m) {
-                res[k] = nums2[j++];
-            } else if (j >= n) {
-                res[k] = nums1[i++];
-            } else {
-                if (nums1[i] <= nums2[j]) {
-                    res[k] = nums1[i++];
-                } else {
-                    res[k] = nums2[j++];
-                }
-            }
-        }
-
-        for (int k = 0; k < m + n; k++) {
-            nums1[k] = res[k];
-        }
-    }
-}
---
+++ LeetCode/0088. Merge Sorted Array/src/Solution1.java
@@ -0,0 +1,35 @@
+/**
+ * @author changleamazing
+ * @date 2019/11/23 12:31
+ **/
+/*
+很容易想到归并，所以使用一个额外数组存放。
+
+时间复杂度：O(n)
+空间复杂度：O(n)
+ */
+class Solution1 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] res = new int[m + n];
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < res.length; k++) {
+            if (i >= m) {
+                res[k] = nums2[j++];
+            } else if (j >= n) {
+                res[k] = nums1[i++];
+            } else {
+                if (nums1[i] <= nums2[j]) {
+                    res[k] = nums1[i++];
+                } else {
+                    res[k] = nums2[j++];
+                }
+            }
+        }
+
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = res[k];
+        }
+    }
+}
