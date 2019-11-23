047c1ff330cb4c52b1aa3ad3b949e5e7e7e3bdb3
changleAmazing <changle0331@gmail.com>
2019-11-23 12:48:51 +0800

    0149. Max Points on a Line V1.2

--- LeetCode/0149. Max Points on a Line/src/Solution.java
+++ LeetCode/0149. Max Points on a Line/src/Solution.java
@@ -9,85 +9,52 @@ import java.util.Map;
 
 /**
  * 同一条线上的任意两点的斜率相同。
- * 将斜率相同的两个点放在一起。这个题目考虑到浮点数的精度问题，不能只把斜率作为 Map 的key来存储。
- * 所以我们使用一个嵌套 Map ，以便于存放点的坐标以及斜率的次数。
+ * 将斜率相同的两个点放在一起
  *
  * 时间复杂度: O(n^2)
  * 空间复杂度: O(n)
  */
 public class Solution {
 
-    class Point {
-
-        int x;
-        int y;
-
-        Point() {
-            x = 0;
-            y = 0;
-        }
-
-        Point(int a, int b) {
-            x = a;
-            y = b;
-        }
-    }
-
-
-    public int maxPoints(Point[] points) {
-        if (points == null) {
-            return 0;
-        }
-        if (points.length <= 2) {
+    public int maxPoints(int[][] points) {
+        if (points.length < 3) {
             return points.length;
         }
 
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<Integer, Map<Integer, Integer>>();
         int result = 0;
+        // 遍历每个点
         for (int i = 0; i < points.length; i++) {
-            map.clear();
-            int overlap = 0, max = 0;
+            int duplicate = 0;
+            int max = 0;
+            HashMap<String, Integer> map = new HashMap<>();
             for (int j = i + 1; j < points.length; j++) {
-                int x = points[j].x - points[i].x;
-                int y = points[j].y - points[i].y;
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
                 if (x == 0 && y == 0) {
-                    overlap++;
+                    duplicate++;
                     continue;
                 }
-                int gcd = generateGCD(x, y);
-                if (gcd != 0) {
-                    x /= gcd;
-                    y /= gcd;
-                }
-
-                if (map.containsKey(x)) {
-                    if (map.get(x).containsKey(y)) {
-                        map.get(x).put(y, map.get(x).get(y) + 1);
-                    } else {
-                        map.get(x).put(y, 1);
-                    }
-                } else {
-                    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-                    m.put(y, 1);
-                    map.put(x, m);
-                }
-                max = Math.max(max, map.get(x).get(y));
+                //进行约分
+                int gcd = gcd(x, y);
+                x = x / gcd;
+                y = y / gcd;
+                String key = x + "@" + y;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                max = Math.max(max, map.get(key));
             }
-            result = Math.max(result, max + overlap + 1);
+            //1 代表当前考虑的点，duplicate 代表和当前的点重复的点
+            result = Math.max(result, max + duplicate + 1);
         }
         return result;
-
-
     }
 
-    private int generateGCD(int a, int b) {
-
-        if (b == 0) {
-            return a;
-        } else {
-            return generateGCD(b, a % b);
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
-
+        return a;
     }
 }
 
