c2b56c255a8b0634b26aba8db3ef3edbea298a56
changleAmazing <changle0331@gmail.com>
2019-11-23 12:16:29 +0800

    0877. Stone Game V1.0

---
+++ LeetCode/0877. Stone Game/src/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * @author changleamazing
+ * @date 2019/11/23 12:16
+ * source: https://leetcode.com/problems/stone-game/
+ **/
+public class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
