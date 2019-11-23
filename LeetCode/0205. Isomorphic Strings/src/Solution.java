63533bbcc248f87f79c8a83796ddd2a2b123a9d0
changleAmazing <changle0331@gmail.com>
2019-11-23 13:42:43 +0800

    0205. Isomorphic Strings V1.1

--- LeetCode/0205. Isomorphic Strings/src/Solution.java
+++ LeetCode/0205. Isomorphic Strings/src/Solution.java
@@ -7,22 +7,25 @@ import java.util.Map;
  * Source: https://leetcode.com/problems/isomorphic-strings/description/
  */
 
-/*
-判断是不是同样结构的字符串。通过映射来判断。
-关于 put 方法：如果 map 中之前存在该 key，就会返回之前的 value。
-这样可以一直保证两个字符串的映射一一匹配。
-
-时间复杂度: O(n)
-空间复杂度: O(n)
+/**
+ * 判断是不是同样结构的字符串。通过映射来判断。
+ * 关于 put 方法：如果 map 中之前存在该 key，就会返回之前的 value。
+ * 这样可以一直保证两个字符串的映射一一匹配。
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
  */
 public class Solution {
+
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        Map index = new HashMap();
-        for (int i=0; i<s.length(); ++i)
-            if (index.put(s.charAt(i), i) != index.put(t.charAt(i), i))
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.indexOf(ch1[i]) != t.indexOf(ch2[i])) {
                 return false;
+            }
+        }
         return true;
     }
 }
