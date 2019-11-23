4c7cdb81c39044f5a4542c6900b0313f1361cc6a
changleAmazing <changle0331@gmail.com>
2019-11-23 13:58:24 +0800

    0344. Reverse String V1.1

--- LeetCode/0344. Reverse String/src/Solution.java
+++ LeetCode/0344. Reverse String/src/Solution.java
@@ -4,35 +4,23 @@
  * Source: https://leetcode.com/problems/reverse-string/description/
  */
 
-/*
-使用对撞指针。交换相对位置的字符。
-
-时间复杂度: O(n)
-空间复杂度: O(n)
+/**
+ * 使用对撞指针。交换相对位置的字符。
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
  */
 public class Solution {
-    public String reverseString(String s) {
-        char[] arr = s.toCharArray();
-        int i = 0,j = arr.length - 1;
+
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
         while (i < j) {
-            char temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
-        return new String(arr);
     }
 }
 
-
-/*
-直接调用 jdk 中的方法
- */
-class Solution1{
-    public String reverseString(String s) {
-        StringBuffer sb = new StringBuffer(s);
-        return sb.reverse().toString();
-    }
-
-}
