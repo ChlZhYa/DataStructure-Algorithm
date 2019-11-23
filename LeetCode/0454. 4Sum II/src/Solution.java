cb5402296cf609ec2897f0a23379dca357f7fffb
changleAmazing <changle0331@gmail.com>
2019-11-23 14:05:29 +0800

    0454. 4Sum II V1.1

--- LeetCode/0454. 4Sum II/src/Solution.java
+++ LeetCode/0454. 4Sum II/src/Solution.java
@@ -6,31 +6,31 @@ import java.util.HashMap;
  * Source: https://leetcode.com/problems/4sum-ii/description/
  */
 
-/*
-将 C + D 的和放入到 map 中。确定 A 中的数，然后遍历 B 集合，
-
-时间复杂度: O(n^3)
-空间复杂度: O(n)
+/**
+ * 将 C + D 的和放入到 map 中。确定 A 中的数，然后遍历 B 集合，
+ *
+ * 时间复杂度: O(n^3)
+ * 空间复杂度: O(n)
  */
 public class Solution {
 
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < C.length; i++) {
-            for (int j = 0; j < D.length; j++) {
-                if (map.containsKey(C[i] + D[j])) {
-                    map.put(C[i] + D[j], map.get(C[i] + D[j]) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int element : C) {
+            for (int value : D) {
+                if (map.containsKey(element + value)) {
+                    map.put(element + value, map.get(element + value) + 1);
                 } else {
-                    map.put(C[i] + D[j], 1);
+                    map.put(element + value, 1);
                 }
             }
         }
 
         int res = 0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                if (map.containsKey(-A[i] - B[j])) {
-                    res = res + map.get(-A[i] - B[j]);
+        for (int item : A) {
+            for (int value : B) {
+                if (map.containsKey(-item - value)) {
+                    res = res + map.get(-item - value);
                 }
             }
         }
