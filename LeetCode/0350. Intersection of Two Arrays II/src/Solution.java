89d2fc94dd241965d929f9d5db8ce63b83938c45
changleAmazing <changle0331@gmail.com>
2019-11-23 14:02:20 +0800

    0350. Intersection of Two Arrays II V1.1

--- LeetCode/0350. Intersection of Two Arrays II/src/Solution.java
+++ LeetCode/0350. Intersection of Two Arrays II/src/Solution.java
@@ -9,34 +9,32 @@ import java.util.Map;
  * Source: https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
  */
 
-/*
-将一个数组遍历一遍，放入 map 中。然后遍历第二个数组。
-如果元素对应的 value > 0，说明这个数是共同元素。
-
-时间复杂度: O(n)
-空间复杂度: O(n)
+/**
+ * 将一个数组遍历一遍，放入 map 中。然后遍历第二个数组。
+ * 如果元素对应的 value > 0，说明这个数是共同元素。
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
  */
 public class Solution {
+
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i: nums1){
-            if(map.get(i) != null)
-                map.put(i,map.get(i) + 1);
-            else
-                map.put(i,1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            map.merge(i, 1, Integer::sum);
         }
 
         List<Integer> list = new ArrayList<>();
-        for(int i = 0;i < nums2.length;i++){
-            if(map.get(nums2[i]) != null && map.get(nums2[i]) > 0 ){
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.get(nums2[i]) != null && map.get(nums2[i]) > 0) {
                 list.add(nums2[i]);
-                map.put(nums2[i],map.get(nums2[i]) - 1);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
             }
         }
 
         int[] res = new int[list.size()];
         int index = 0;
-        for(Integer i: list){
+        for (Integer i : list) {
             res[index++] = i;
         }
         return res;
