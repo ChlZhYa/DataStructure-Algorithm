6918ca05e6f2304ea8dc66e545d7684da2568eb4
changleAmazing <changle0331@gmail.com>
2019-11-23 12:37:07 +0800

    0125. Valid Palindrome V1.1

--- LeetCode/0125. Valid Palindrome/src/Solution.java
+++ LeetCode/0125. Valid Palindrome/src/Solution.java
@@ -4,10 +4,10 @@
  * Source: https://leetcode.com/problems/valid-palindrome/description/
  */
 
-/*
-对撞指针判断对称的两个字符，如果是特殊符号则继续移动。
-
-依次判断。当 两个指针相遇时，说明这个字符串是回文串。
+/**
+ * 对撞指针判断对称的两个字符，如果是特殊符号则继续移动。
+ *
+ * 依次判断。当 两个指针相遇时，说明这个字符串是回文串。
  */
 public class Solution {
 
