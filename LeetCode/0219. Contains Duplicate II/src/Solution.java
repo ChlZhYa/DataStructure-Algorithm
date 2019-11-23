7b70f96f2afe2df1b37d3e15e8c2c331d43fc276
changleAmazing <changle0331@gmail.com>
2019-11-23 13:50:08 +0800

    0219. Contains Duplicate II V1.1

--- LeetCode/0219. Contains Duplicate II/src/Solution.java
+++ LeetCode/0219. Contains Duplicate II/src/Solution.java
@@ -7,22 +7,25 @@ import java.util.Set;
  * Source: https://leetcode.com/problems/contains-duplicate-ii/description/
  */
 
-/*
-遍历数组，将元素放入集合中，保持集合中的元素个数不超过 k + 1个。
-判断最新加入来的元素是否在 set 中出现过。
-
-时间复杂度: O(n)
-空间复杂度: O(n)
+/**
+ * 遍历数组，将元素放入集合中，保持集合中的元素个数不超过 k + 1个。
+ * 判断最新加入来的元素是否在 set 中出现过。
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
  */
 public class Solution {
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
-        for(int i = 0;i < nums.length;i++){
-            if(!set.add(nums[i]))
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
                 return true;
+            }
 
-            if(set.size() == k + 1)
+            if (set.size() == k + 1) {
                 set.remove(nums[i - k]);
+            }
         }
         return false;
     }
