6dc2f8e4070f933a2b0c81dc5e64b118a0f8ca90
changleAmazing <changle0331@gmail.com>
2019-11-23 14:03:20 +0800

    0438. Find All Anagrams in a String V1.1

--- LeetCode/0438. Find All Anagrams in a String/src/Solution.java
+++ LeetCode/0438. Find All Anagrams in a String/src/Solution.java
@@ -10,32 +10,36 @@ import java.util.Map;
  * Source: https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
  */
 
-/*
-使用滑动窗口。
-count 记录还有几个 p 中的字符应该出现。
-
-时间复杂度: O(len(p) + len(s))
-空间复杂度: O(len(p))
+/**
+ * 使用滑动窗口。
+ * count 记录还有几个 p 中的字符应该出现。
+ *
+ * 时间复杂度: O(len(p) + len(s))
+ * 空间复杂度: O(len(p))
  */
 public class Solution {
+
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
         int[] ch = new int[256];
-        for(char c:p.toCharArray()){
+        for (char c : p.toCharArray()) {
             ch[c]++;
         }
 
-        int i = 0,j = 0;
+        int i = 0, j = 0;
         int count = p.length();
-        while(i < s.length() && j < s.length()){
-            if(ch[s.charAt(j++)]-- > 0)
+        while (i < s.length() && j < s.length()) {
+            if (ch[s.charAt(j++)]-- > 0) {
                 count--;
+            }
 
-            if(count == 0)
+            if (count == 0) {
                 list.add(i);
+            }
 
-            if(j - i == p.length() && ch[s.charAt(i++)]++ >= 0)
+            if (j - i == p.length() && ch[s.charAt(i++)]++ >= 0) {
                 count++;
+            }
         }
 
         return list;
