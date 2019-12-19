a6d863d01a8fb09be7e2b39a40af30ee1d3260fe
张常乐 <changle0331@gmail.com>
2019-12-19 17:25:50 +0800

    Create Solution.java

---
+++ LeetCode/0237. Delete Node in a Linked List/src/Solution.java
@@ -0,0 +1,12 @@
+/**
+ * @author Changle
+ * @date 2019/12/19 17:25
+ * source: https://leetcode.com/problems/delete-node-in-a-linked-list/
+ */
+
+class Solution {
+    public void deleteNode(ListNode node) {
+           node.val = node.next.val;
+           node.next = node.next.next;
+    }
+}
