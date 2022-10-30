87b5821d0e1373d887aaf19c61fdba19458a81c5
ChlZhYa <16614056@qq.com>
2022-10-30 16:06:11 +0800

    add L0784 solution

---
+++ LeetCode/L0784. Letter Case Permutation/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> res = new ArrayList<>();
+        res.add("");
+        for(int i = 0;i < s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isLetter(c)){
+                res = res.stream().map(o -> {
+                    List<String> list = new ArrayList<>();
+                    list.add(o + Character.toLowerCase(c));
+                    list.add(o + Character.toUpperCase(c));
+                    return list;
+                }).flatMap(List::stream).collect(Collectors.toList());
+            }else{
+                res = res.stream().map(o -> o + c).collect(Collectors.toList());
+            }
+        }
+        return res;
+    }
+}
