0da4cd910d859a3d1741cee90cfa4fd814eb8a9e
ChlZhYa <16614056@qq.com>
2022-11-05 06:50:29 +0800

    add L0001 solution

---
+++ LeetCode/L0001. Two Sum/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0;i < nums.length;i++){
+            if (hashMap.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = hashMap.get(target - nums[i]);
+                return res;
+            }else{
+                hashMap.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
